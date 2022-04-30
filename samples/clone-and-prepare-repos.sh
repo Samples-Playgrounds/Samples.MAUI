3362c4b8386176911de992f167e7fbcf50ca6051
moljac <mcvjetko@holisticware.net>
2022-04-30 11:29:43 +0200

    Update clone-and-prepare-repos.sh

--- samples/clone-and-prepare-repos.sh
+++ samples/clone-and-prepare-repos.sh
@@ -1,39 +1,77 @@
 #!/bin/bash
 
 
-dotnet tool uninstall -g \
-    redth.net.maui.check
-dotnet tool install -g \
-    redth.net.maui.check
+dotnet tool \
+    uninstall -g \
+        redth.net.maui.check
+dotnet tool \
+    install -g \
+        redth.net.maui.check
 maui-check
 
-rm -fr maui/
+rm -fr  ./external-samples/
+mkdir   ./external-samples/
+
+rm -fr ./external-samples/maui/
 git clone \
     --recursive \
-    https://github.com/dotnet/maui.git
+    https://github.com/dotnet/maui.git \
+    ./external-samples/maui/
 
-rm -fr maui-samples/
+rm -fr ./external-samples/maui-samples/
 git clone \
     --recursive \
-    https://github.com/dotnet/maui-samples.git
+    https://github.com/dotnet/maui-samples.git \
+    ./external-samples/maui-samples/
 
-rm -fr MAUIFriendsListWithMVVM/
+rm -fr ./external-samples/dotnet-podcasts/
 git clone \
     --recursive \
-    https://github.com/jaysonragasa/MAUIFriendsListWithMVVM.git
-
-
-rm -fr BlazorWeather/
+    https://github.com/microsoft/dotnet-podcasts.git \
+    ./external-samples/dotnet-podcasts/
+    
+rm -fr ./external-samples/MAUIFriendsListWithMVVM/
 git clone \
     --recursive \
-    https://github.com/danroth27/BlazorWeather.git
+    https://github.com/jaysonragasa/MAUIFriendsListWithMVVM.git \
+    ./external-samples/MAUIFriendsListWithMVVM/
 
-rm -fr WeatherTwentyOne/
+rm -fr ./external-samples/BlazorWeather/
 git clone \
     --recursive \
-    https://github.com/davidortinau/WeatherTwentyOne.git
+    https://github.com/danroth27/BlazorWeather.git \
+    ./external-samples/BlazorWeather/
 
-cd WeatherTwentyOne/src/
+rm -fr ./external-samples/WeatherTwentyOne/
+git clone \
+    --recursive \
+    https://github.com/davidortinau/WeatherTwentyOne.git \
+    ./external-samples/WeatherTwentyOne/
+
+rm -fr ./external-samples/jsuarezruiz--dotnet-maui-samples/
+git clone \
+    --recursive \
+    https://github.com/jsuarezruiz/dotnet-maui-samples.git \
+    ./external-samples/jsuarezruiz--dotnet-maui-samples/ 
+
+rm -fr ./external-samples/beto-rodriguez--LiveCharts2-master/
+git clone \
+    --recursive \
+    --branch master \
+    https://github.com/beto-rodriguez/LiveCharts2.git \
+    ./external-samples/beto-rodriguez--LiveCharts2-master/
+
+# https://github.com/beto-rodriguez/LiveCharts2
+rm -fr ./external-samples/beto-rodriguez--LiveCharts2-dev/
+git clone \
+    --recursive \
+    --branch dev \
+    https://github.com/beto-rodriguez/LiveCharts2.git \
+    ./external-samples/beto-rodriguez--LiveCharts2-dev/
+
+
+
+
 
 dotnet new classlib \
     --framework netstandard2.0 \
@@ -48,6 +86,11 @@ dotnet sln \
     add \
     WeatherTwentyOne.DomainBusinessLogic/WeatherTwentyOne.DomainBusinessLogic.csproj \
     --solution-folder DomainBusinessLogic \
+
+
+
+
+
 
 
 cd BlazorWeather/
