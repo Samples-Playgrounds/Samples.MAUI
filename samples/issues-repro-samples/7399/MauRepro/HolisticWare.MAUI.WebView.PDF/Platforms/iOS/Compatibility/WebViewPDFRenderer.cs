425325cd54da3716db4f4e6d1d2cd968b212e303
moljac <mcvjetko@holisticware.net>
2023-02-24 18:18:00 +0100

    renderers

---
+++ samples/issues-repro-samples/7399/MauRepro/HolisticWare.MAUI.WebView.PDF/Platforms/iOS/Compatibility/WebViewPDFRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Net;
+
+using Foundation;
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using HolisticWare.XamarinForms.WebView.PDF;
+using HolisticWare.XamarinForms.WebView.PDF.Platforms.iOS;
+
+[assembly: ExportRenderer(typeof(WebViewPDF), typeof(WebViewPDFRenderer))]
+namespace HolisticWare.XamarinForms.WebView.PDF.Platforms.iOS
+{
+    public class WebViewPDFRenderer : ViewRenderer<WebViewPDF, UIWebView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<WebViewPDFRenderer> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                SetNativeControl(new UIWebView());
+            }
+            if (e.OldElement != null)
+            {
+                // Cleanup
+            }
+            if (e.NewElement != null)
+            {
+                WebViewPDF customWebView = Element as WebViewPDF;
+                string fileName = Path.Combine(NSBundle.MainBundle.BundlePath, string.Format("Content/{0}", WebUtility.UrlEncode(customWebView.Uri)));
+                Control.LoadRequest(new NSUrlRequest(new NSUrl(fileName, false)));
+                Control.ScalesPageToFit = true;
+            }
+        }
+    }
+}
