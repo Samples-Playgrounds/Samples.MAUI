d950dff63a7129be0085c5bd10f17022981016b2
moljac <mcvjetko@holisticware.net>
2023-03-29 20:08:50 +0200

    deep link crash initial commit

---
+++ samples/issues-repro-samples/11684/related/MauiBug_DeepLinkCrash-master/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+namespace MauiBug_DeepLinkCrash;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    protected override async void OnAppLinkRequestReceived(Uri uri)
+    {
+        base.OnAppLinkRequestReceived(uri);
+
+        
+    }
+}
