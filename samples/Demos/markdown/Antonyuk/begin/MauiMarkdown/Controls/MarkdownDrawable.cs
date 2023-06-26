ffe9c8e3a92944873b06a000e1ba768c29a58b52
moljac <mcvjetko@holisticware.net>
2023-06-26 17:05:32 +0200

    markdown sample

---
+++ samples/Demos/markdown/Antonyuk/begin/MauiMarkdown/Controls/MarkdownDrawable.cs
@@ -0,0 +1,51 @@
+﻿namespace MauiMarkdown;
+
+using Markdig;
+using Microsoft.Maui.Graphics.Text;
+using Microsoft.Maui.Graphics.Text.Renderer;
+using Font = Microsoft.Maui.Graphics.Font;
+
+public class MarkdownDrawable : IDrawable
+{
+	private readonly string text;
+	private readonly Color fontColor;
+	private readonly float fontSize;
+	private readonly int markdownWidth;
+	private readonly int markdownHeight;
+
+	public MarkdownDrawable(string text, Color fontColor, double fontSize, double markdownWidth, double markdownHeight)
+	{
+		this.text = text;
+		this.fontColor = fontColor;
+		this.fontSize = (float)fontSize;
+		this.markdownWidth = (int)markdownWidth;
+		this.markdownHeight = (int)markdownHeight;
+	}
+
+	public void Draw(ICanvas canvas, RectF dirtyRect)
+	{
+		canvas.Font = Font.Default;
+		canvas.FontSize = fontSize;
+		canvas.FontColor = fontColor;
+		var attributedText = Read(text);
+#if WINDOWS
+		canvas.DrawString(attributedText.Text, 0, 0, Math.Max(0, markdownWidth), Math.Max(0, markdownHeight), HorizontalAlignment.Left, VerticalAlignment.Top);
+#else
+		canvas.DrawText(attributedText, 0, 0, markdownWidth, markdownHeight);
+#endif
+	}
+
+	private static IAttributedText Read(string text)
+	{
+		var renderer = new AttributedTextRenderer();
+		renderer.ObjectRenderers.Add(new MauiCodeInlineRenderer());
+		renderer.ObjectRenderers.Add(new MauiCodeBlockRenderer());
+		renderer.ObjectRenderers.Add(new MauiHeadingRenderer());
+		var builder = new MarkdownPipelineBuilder()
+					  .UseEmojiAndSmiley()
+					  .UseEmphasisExtras();
+		var pipeline = builder.Build();
+		Markdown.Convert(text, renderer, pipeline);
+		return renderer.GetAttributedText();
+	}
+}
---
+++ samples/Demos/markdown/Antonyuk/begin/MauiMarkdown/Controls/MauiCodeBlockRenderer.cs
@@ -0,0 +1,33 @@
+﻿namespace MauiMarkdown;
+
+using Markdig.Syntax;
+using Microsoft.Maui.Graphics.Text;
+using Microsoft.Maui.Graphics.Text.Renderer;
+
+public class MauiCodeBlockRenderer : AttributedTextObjectRenderer<CodeBlock>
+{
+	protected override void Write(
+		AttributedTextRenderer renderer,
+		CodeBlock codeBlock)
+	{
+		var start = renderer.Count;
+		var attributes = new TextAttributes();
+		attributes.SetBackgroundColor("#f5f2f0");
+		var randomColor = new Color(Random.Shared.Next(100), Random.Shared.Next(100), Random.Shared.Next(100));
+		attributes.SetForegroundColor(randomColor.ToArgbHex());
+
+		if (codeBlock.Lines.Lines != null)
+		{
+			var lines = codeBlock.Lines;
+			var slices = lines.Lines;
+			for (int i = 0; i < lines.Count; i++)
+			{
+				renderer.Write(ref slices[i].Slice);
+				renderer.WriteLine();
+			}
+		}
+
+		var length = renderer.Count - start;
+		renderer.Call("AddTextRun", start, length, attributes);
+	}
+}
