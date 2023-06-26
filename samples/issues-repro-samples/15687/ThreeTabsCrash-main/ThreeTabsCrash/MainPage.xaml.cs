7aad943d0b66c375c37c2d55770479a83e1d4cce
moljac <mcvjetko@holisticware.net>
2023-06-26 17:07:32 +0200

    issue 15687 ThreeTabsCrash

---
+++ samples/issues-repro-samples/15687/ThreeTabsCrash-main/ThreeTabsCrash/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ThreeTabsCrash;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked( object sender, EventArgs e )
+    {
+        count++;
+
+        if ( count == 1 )
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce( CounterBtn.Text );
+    }
+
+    private void OnTabsClicked( object sender, EventArgs e )
+    {
+        App.Current.MainPage.Navigation.PushAsync( new MyTabbedPage() );
+    }
+}
