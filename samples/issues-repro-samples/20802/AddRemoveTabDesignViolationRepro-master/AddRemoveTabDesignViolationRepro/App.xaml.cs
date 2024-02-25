4d929b65093c0349e1c77007787c9347a6ed2666
moljac <mcvjetko@holisticware.net>
2024-02-25 23:22:20 +0100

    issue 20802

---
+++ samples/issues-repro-samples/20802/AddRemoveTabDesignViolationRepro-master/AddRemoveTabDesignViolationRepro/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AddRemoveTabDesignViolationRepro;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
