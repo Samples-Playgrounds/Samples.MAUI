8a17a76151681ac65cd16e01ec7257d42a9c0ba9
moljac <mcvjetko@holisticware.net>
2024-10-23 11:41:22 +0200

    24557 Android OnKeyUp

---
+++ samples/issues-repro-samples/24557/dotnet-test/net8.0/AppMAUI.mauiapp.net8.0/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace AppMAUI.mauiapp.net8._0
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            UserAppTheme = PlatformAppTheme;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "AppMAUI.mauiapp.net8._0";
+            return window;
+        }
+    }
+}
---
+++ samples/issues-repro-samples/24557/dotnet-test/net8.0/AppMAUI.mauiapp.net8.0/Platforms/Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+
+namespace AppMAUI.mauiapp.net8._0;
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+public class MainActivity : MauiAppCompatActivity
+{
+    public override bool OnKeyUp([GeneratedEnum] Android.Views.Keycode keyCode, KeyEvent e)
+    {
+        bool wasHandled = false; //PlatformService.HandleOnKeyUp(keyCode, e);
+        if (!wasHandled)
+            return base.OnKeyUp(keyCode, e);
+        else
+            return true;
+    }        
+        
+}
+
