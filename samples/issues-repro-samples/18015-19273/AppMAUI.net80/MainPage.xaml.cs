d299e09db559136c8d6ae0a8f7b932de06bb343f
moljac <mcvjetko@holisticware.net>
2024-10-23 13:40:40 +0200

    picker maccatalyst initial commit

---
+++ samples/issues-repro-samples/18015-19273/AppMAUI.net80/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AppMAUI.net80;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+	}
+}
---
+++ samples/issues-repro-samples/18015-19273/AppMAUI.net90/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AppMAUI.net90;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
