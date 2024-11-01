fd5881be1893d13f0a4988fe4b0fc5e88d3b82e2
moljac <mcvjetko@holisticware.net>
2024-11-01 10:14:25 +0100

    25585 added net8.0 and net9.0

---
+++ samples/issues-repro-samples/25585/net9.0/MauiTest-main/Pages/MainFlyoutPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace CleanApp.Pages
+{
+    public class MainFlyoutPage : FlyoutPage
+    {
+        public MainFlyoutPage()
+        {
+            Flyout = new MenuPage();
+            Detail = new NavigationPage(new AnnouncementPage());
+
+        }
+    }
+}
