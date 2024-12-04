0af74cbffc674a4b7a4ab28b9df68580098342dd
moljac <mcvjetko@holisticware.net>
2024-12-04 13:20:03 +0100

    Update MainPage.xaml.cs

--- samples/issues-repro-samples/26167/net9.0/FileNotFoundImage-main/FileNotFoundImage/MainPage.xaml.cs
+++ samples/issues-repro-samples/26167/net9.0/FileNotFoundImage-main/FileNotFoundImage/MainPage.xaml.cs
@@ -11,20 +11,28 @@ public partial class MainPage : ContentPage
 
     private async void OnCounterClicked(object sender, EventArgs e)
     {
-
-        var stream = await FileSystem.OpenAppPackageFileAsync("dotnet_bot.png");
+        var s1 = await FileSystem.OpenAppPackageFileAsync("dotnet_bot.png");
         
-        using var memoryStream = new MemoryStream();
-        await stream.CopyToAsync(memoryStream);
+        using var ms1 = new MemoryStream();
+        await s1.CopyToAsync(ms1);
+        
+        
+        var byteImg1 = ms1.ToArray();
+        
+        var s2 = await FileSystem.OpenAppPackageFileAsync("dotnet_bot_raw.png");
+        
+        using var ms2 = new MemoryStream();
+        await s2.CopyToAsync(ms1);
+        
+        var byteImg2 = ms1.ToArray();
 
-        var byteImg = memoryStream.ToArray();
-        // count++;
-        //
-        // if (count == 1)
-        //     CounterBtn.Text = $"Clicked {count} time";
-        // else
-        //     CounterBtn.Text = $"Clicked {count} times";
-        //
-        // SemanticScreenReader.Announce(CounterBtn.Text);
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
     }
 }
