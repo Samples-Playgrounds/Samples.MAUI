ce851a64c9f4da7b276f1d2f65a2057cc775e35c
moljac <mcvjetko@holisticware.net>
2025-03-11 00:17:39 +0100

    21525

---
+++ samples/issues-repro-samples/21525/TestNet8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestNet8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ samples/issues-repro-samples/21525/TestNet8/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNet8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
