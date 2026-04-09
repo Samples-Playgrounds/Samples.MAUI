edfd5dd84f3fbaa3f212f97f4c36557f9cd1e58e
moljac <mcvjetko@holisticware.net>
2026-04-09 23:08:57 +0200

    repro sample

---
+++ AppMAUI.20230316222847/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppMAUI._20230316222847;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppMAUI.20230316222847/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMAUI._20230316222847;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
